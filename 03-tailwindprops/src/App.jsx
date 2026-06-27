79581e9a56fd4935482de773f54195d6f0f436b6
user.name "manthan2125 <user.name "manthan2125>
2026-06-27 22:52:15 +0530

    props learning

--- 03-tailwindprops/src/App.jsx
+++ 03-tailwindprops/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card from './components/Card';
 
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h2 className='bg-slate-600 p-4 rounded-lg'>Tailwind Test</h2>
+      <Card username="manthan" />
+      <Card username="manthankirti" />
     </>
   )
 }
