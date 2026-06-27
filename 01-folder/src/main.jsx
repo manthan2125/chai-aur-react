4034fb6ed88fa5853545567500c92f70a7617ec2
user.name "manthan2125 <user.name "manthan2125>
2026-06-27 14:51:32 +0530

    creating reactElement

--- 01-folder/src/main.jsx
+++ 01-folder/src/main.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App | Manthan</h1>
+        </div>
+    )
+}
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    'Click me to Visit google | ManthanKirti'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
     <App />
-    // App()   Same thing
+    // <MyApp />
+    // reactElement
 )
