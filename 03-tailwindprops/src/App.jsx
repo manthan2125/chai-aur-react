50ab1e333a41310643c1e3cf5e0c5311dcb6c6e2
user.name "manthan2125 <user.name "manthan2125>
2026-06-27 21:53:49 +0530

    initial files

---
+++ 03-tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='bg-slate-600 p-4 rounded-lg'>Tailwind Test</h2>
+    </>
+  )
+}
+
+export default App
